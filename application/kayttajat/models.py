ba8908dbaca621c4e0a25ad28a5a7ac004128893
jmlii <44909874+jmlii@users.noreply.github.com>
2020-01-19 14:47:17 +0200

    Lisätty Käyttäjä-luokkaan tieto aktiivisuudesta ja päivitetty dokumentteja vastaavasti.

--- application/kayttajat/models.py
+++ application/kayttajat/models.py
@@ -5,12 +5,14 @@ class Kayttaja(db.Model):
     etunimi = db.Column(db.String(36), nullable=False)
     sukunimi = db.Column(db.String(36), nullable=False)
     kayttooikeustaso = db.Column(db.String(18), nullable=False)
+    aktiivinen = db.Column(db.Boolean, nullable=False, default=True)
     luontipvm = db.Column(db.DateTime, default=db.func.current_timestamp())
     muokkauspvm = db.Column(db.DateTime, default=db.func.current_timestamp(), 
     onupdate=db.func.current_timestamp())
     loppupvm = db.Column(db.DateTime)
 
-    def __init__(self, etunimi, sukunimi, kayttooikeustaso):
+    def __init__(self, etunimi, sukunimi, kayttooikeustaso, aktiivinen):
         self.etunimi = etunimi
         self.sukunimi = sukunimi
         self.kayttooikeustaso = kayttooikeustaso
+        self.aktiivinen = aktiivinen
--- application/kayttajat/views.py
+++ application/kayttajat/views.py
@@ -2,21 +2,34 @@ from application import app, db
 from flask import render_template, request
 from application.kayttajat.models import Kayttaja
 
+# Käyttäjien listaaminen
 @app.route("/kayttajat", methods=["GET"])
 def kayttajat_index():
     return render_template("kayttajat/list.html", kayttajat = Kayttaja.query.all())
 
+# Uuden käyttäjän luominen
 @app.route("/kayttajat/new")
 def kayttajat_form():
     return render_template("/kayttajat/new.html")
 
+# Luodun käyttäjän tiedot tietokantaan ja sivulle lisäämisen jälkeen
 @app.route("/kayttajat/created", methods=["POST"])
 def kayttajat_create():
     k = Kayttaja(request.form.get("etunimi"), 
     request.form.get("sukunimi"),
-    request.form.get("kayttooikeustaso"))
-
+    request.form.get("kayttooikeustaso"), aktiivinen=True)
+    
     db.session().add(k)
     db.session().commit()
 
     return "Tietokantaan lisätty: " + k.etunimi + " " + k.sukunimi + ", käyttöoikeustaso " + k.kayttooikeustaso + ", id " + str(k.id)
+
+# Käyttäjän päättäminen
+@app.route("/kayttajat/end<kayttaja_id>/", methods=["POST"])
+def kayttajat_set_end(kayttaja_id): 
+    k = Kayttaja.query.get(kayttaja_id)
+    k.aktiivinen = False
+    k.loppupvm = db.func.current_timestamp()
+    db.session().commit()
+    
+    return "Käyttäjä päätetty järjestelmästä: id " + str(k.id) +", " + k.etunimi + " " + k.sukunimi
