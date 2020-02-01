8b709060d88ba6ffbf88d9cf752f6a14ac9e09b3
jmlii <44909874+jmlii@users.noreply.github.com>
2020-02-01 22:49:39 +0200

    Lisätty abstrakti Base-luokka, tuotu Bootstrap ulkoasuun.

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,18 +1,19 @@
 from application import db
+from application.models import Base 
 
-class User(db.Model):
+class User(Base): #(db.Model):
 
     __tablename__ = "account"
 
-    id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(144), nullable=False)
     last_name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
     account_active = db.Column(db.Boolean, nullable=False, default=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-        onupdate=db.func.current_timestamp())
+    # date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    # date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    #    onupdate=db.func.current_timestamp())
     date_inactivated = db.Column(db.DateTime)
 
     
--- application/wgroups/models.py
+++ application/wgroups/models.py
@@ -1,13 +1,14 @@
 from application import db
+from application.models import Base 
 
-class Wgroup(db.Model):
-    id = db.Column(db.Integer, primary_key=True)   
+class Wgroup(Base):  #(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)   
     name = db.Column(db.String(36), nullable=False)
     authoriser = db.Column(db.String(36), nullable=False)
     active = db.Column(db.Boolean, nullable=False, default=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+    # date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    # date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    # onupdate=db.func.current_timestamp())
     date_ended = db.Column(db.DateTime)
 
     def __init__(self, name, authoriser):
