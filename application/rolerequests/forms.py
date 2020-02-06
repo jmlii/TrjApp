0da372d2c704557c6b2c79811e7be4be5fb568dc
jmlii <44909874+jmlii@users.noreply.github.com>
2020-02-06 18:23:00 +0200

    Rolerequestin lomakkeilta poistettu queryt

--- application/rolerequests/forms.py
+++ application/rolerequests/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
-from wtforms import RadioField, SelectField, StringField, validators
+from wtforms import RadioField, IntegerField, SelectField, StringField, validators
 from application.wgroups.models import Wgroup
 from application.roles.models import Role
 from application.auth.models import User
@@ -12,19 +12,8 @@ class RolerequestForm(FlaskForm):
     request_type = RadioField("Pyynnön tyyppi", 
         [validators.InputRequired()], choices=[('New', 'Uusi rooli'), 
         ('Modify', 'Muokkaus'), ('Remove', 'Poisto')])
-    
-    wgroups = Wgroup.query.order_by(Wgroup.name)
-    wgroup_choices = []
-    for wgroup in wgroups:
-        wgroup_choices.append((wgroup.id, wgroup.name))
-    wgroup_id = SelectField("Työryhmä", choices=wgroup_choices, coerce=int)
-    
-    roles = Role.query.all()
-    role_choices = []
-    for role in roles:
-        role_choices.append((role.id, role.name))
-    role_id = SelectField("Rooli", choices=role_choices, coerce=int)
-
+    wgroup_id = IntegerField("Työryhmän id", [validators.InputRequired()])
+    role_id = SelectField("Rooli", choices=[('1', 'Reader'), ('2', 'Editor'), ('3', 'Manager')], coerce=int)
     justification = StringField("Perustelut", [validators.Length(min=3)])
 
     class Meta:
@@ -37,26 +26,9 @@ class RolerequestForm2(FlaskForm):
     request_type = RadioField("Pyynnön tyyppi", 
         [validators.InputRequired()], choices=[('New', 'Uusi rooli'), 
         ('Modify', 'Muokkaus'), ('Remove', 'Poisto')])
-    
-    accounts = User.query.order_by(User.username)
-    account_choices = []
-    for account in accounts:
-        account_choices.append((account.id, account.username))
-
-    account_id = SelectField("Käyttäjä", choices=account_choices, coerce=int)
-    
-    wgroups = Wgroup.query.order_by(Wgroup.name)
-    wgroup_choices = []
-    for wgroup in wgroups:
-        wgroup_choices.append((wgroup.id, wgroup.name))
-    wgroup_id = SelectField("Työryhmä", choices=wgroup_choices, coerce=int)
-    
-    roles = Role.query.all()
-    role_choices = []
-    for role in roles:
-        role_choices.append((role.id, role.name))
-    role_id = SelectField("Rooli", choices=role_choices, coerce=int)
-
+    account_id = IntegerField("Käyttäjän id", [validators.InputRequired()])
+    wgroup_id = IntegerField("Työryhmän id", [validators.InputRequired()])
+    role_id = SelectField("Rooli", choices=[('1', 'Reader'), ('2', 'Editor'), ('3', 'Manager')], coerce=int)
     justification = StringField("Perustelut", [validators.Length(min=3)])
 
     class Meta:
