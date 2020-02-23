3236ac3857db68aaed208e56cc4abfcd3f9ba6b2
jmlii <44909874+jmlii@users.noreply.github.com>
2020-02-23 23:18:17 +0200

    Käyttäjälistausta muutettu

--- application/auth/views.py
+++ application/auth/views.py
@@ -30,7 +30,7 @@ def auth_logout():
 @login_required(permission="admin")
 def users_index():
     page=request.args.get('page', 1, type=int)
-    users = User.query.order_by("last_name").paginate(page=page, per_page=10, error_out=False)
+    users = User.query.order_by(User.last_name).paginate(page=page, per_page=10, error_out=False)
 
     return render_template("auth/list.html", users=users,
         count_memberships = User.count_memberships(active=1))
